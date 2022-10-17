fc9a9bc89ee5e4dc9361770811d41aadb990d7ad
justicemerrick <justicemer@gmail.com>
2022-10-17 00:22:36 -0700

    add integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,13 +2,41 @@
 """Defines an integer addition function."""
 
 def add_integer(a, b=98):
-    """Return the integer addition of a and b.
+    """Return the integer addition of two numbers.
 
-    Float arguments are typecasted to ints before addition is performed.
+    Args:
+        a (:obj:'int, float'): First number.
+        b (:obj:'int, float', optional): Second number.
 
-    Raises:
-        TypeError: If either of a or b is a non-integer and non-float.
+    Returns:
+        int: addition results.
+
     """
-    if ((not isinstance(a, int) and not isinstance(a, float))):
-        raise TypeError("a must be an integer")
-    return (int(a) + int(b))
+    if type(a) not in (int, float):
+        raise TypeError('a must be an integer')
+
+    if type(b) not in (int, float):
+        raise TypeError('b must be an integer')
+
+    a = convert_to_int(a)
+    b = convert_to_int(b)
+    return a + b
+
+
+def convert_to_int(num)
+    """Cast the data type of num parameter
+
+    Convert a float number to a integer number
+
+    Args:
+        num (:obj:'int, float'): The number to be casted.
+
+    Returns:
+        int: The number casted to integer.
+
+    """
+    if type(num) is float:
+        num = int(num)
+        return num
+
+    return num
