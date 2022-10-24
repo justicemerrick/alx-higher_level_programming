10e281266c652ad59f296f6dcd982a00ee4ff50e
Justice Merrick <106972535+justicemerrick@users.noreply.github.com>
2022-10-24 23:53:48 +0300

    Update 0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def lookup(obj):
-    """
-        Returns a list of attributes and methods in the object
-    """
+
+    """Returns a list of attributes and methods in the object"""
+
     return(dir(obj))
