20e95f09e2f94cf481dcfb35d1be8a620b4da652
Justice Merrick <106972535+justicemerrick@users.noreply.github.com>
2022-10-25 00:38:14 +0300

    Update 0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""Inherits  from a list"""
-
+"""Inherits  from a list."""
 
 def lookup(obj):
     """Return a list of an object's available attributes."""
     return (dir(obj))
-
